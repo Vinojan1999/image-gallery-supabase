f6b4791a4bdbe879349a494eea9d63c9aaf35795
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-05-30 18:59:39 +0530

    updated the changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,147 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { Container, Form, Button, Row, Col, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
+import { v4 as uuidv4 } from 'uuid';
+
+// https://xiiprutvwatfvdvjnbia.supabase.co/storage/v1/object/public/images/28a5f52f-f166-430c-a030-ab666c8ebe86/1ddca3ad-c8e5-43ef-8f80-d15dafd87d3e
+
+const CDNURL = "https://ffrkcwvnegfaicnexliq.supabase.co/storage/v1/object/public/images/";
+
+// CDNURL + user.id + "/" + image.name
+
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [images, setImages] = useState([]);
+
+  const user = useUser();
+  const supabase = useSupabaseClient();
+
+  async function getImages() {
+    const { data, error } = await supabase
+      .storage
+      .from('images')
+      .list(user?.id + '/', {
+        limit: 100,
+        offset: 0,
+        sortBy: { column: "name", order: "asc" }
+      })
+      // data: [image1, image2, image3]
+      // image1: { name: "imagename.jpg" }
+
+      if(data !== null) {
+        setImages(data);
+      }else {
+        alert("Error Loading images");
+        console.log(error);
+      }
+  }
+
+  useEffect(() => {
+    if(user) {
+      getImages();
+    }
+  }, [user])
+  
+
+  async function magicLinkLogin() {
+    const { data, error } = await supabase.auth.signInWithOtp({
+      email: email
+    });
+
+    if(error) {
+      alert("Error communicating with supabase, make sure to use a real email address!");
+      console.log(error);
+    } else {
+      alert("Check your email for a Supabase Magic Link to log in!");
+    }
+  }
+
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+  }
+
+  async function uploadImage(e) {
+    let file = e.target.files[0];
+
+    const { data, error } = await supabase
+      .storage
+      .from('images')
+      .upload(user.id + '/' + uuidv4(), file)
+
+    if(data) {
+      getImages();
+    } else {
+      console.log(error);
+    }
+  }
+
+  async function deleteImage(imageName) {
+    const { error } = await supabase
+      .storage
+      .from('images')
+      .remove([user.id + '/' + imageName])
+
+    if(error) {
+      alert(error)
+    } else {
+      getImages();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container align='center' className='container-sm mt-4'>
+      {/* 
+        If the User exist: show them the images / upload images page
+        If they don't exist: show them the login page
+       */}
+      { user === null ?
+        <>
+          <h1>Welcome to the Imagewall</h1>
+          <Form>
+            <Form.Group className="mb-3" style={{maxWidth: "500px"}}>
+              <Form.Label>Enter an email to sign in with a Supabase Magic Link</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" onClick={() => magicLinkLogin()}>
+              Get Magic Link
+            </Button>
+          </Form>
+        </>
+      :
+        <>
+          <h1>Your ImageWall</h1>
+          <Button onClick={() => signOut()}>Sign Out</Button>
+          <p>Current user: {user.email}</p>
+          <p>Use the Choose File button below to upload an image to your gallery</p>
+          <Form.Group className="mb-3" style={{ maxWidth: "500px" }}>
+            <Form.Control type="file" accept="image/png, image/jpeg" onChange={(e) => uploadImage(e)}/>
+          </Form.Group>
+          <hr />
+          <h3>Your Images</h3>
+          <Row xs={1} md={3} className='g-4'>
+            {images.map((image) => {
+              return (
+                <Col>
+                  <Card>
+                    <Card.Img variant='top' src={CDNURL + user.id + "/" + image.name} />
+                    <Card.Body>
+                      <Button variant='danger' onClick={() => deleteImage(image.name)} >Delete Image</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )
+            })}
+          </Row>
+        </>
+      }
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient("https://ffrkcwvnegfaicnexliq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmcmtjd3ZuZWdmYWljbmV4bGlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU0NTIwMjgsImV4cCI6MjAwMTAyODAyOH0.w5gnVeg2AquYJJfQqLgBNsW90MQhGp2DUSybqLf1V9Y")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
